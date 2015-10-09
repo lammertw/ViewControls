ddf24d561895e14495c3108695234c4b1602076c
Lammert Westerhoff <westerhoff@gmail.com>
2015-10-09 16:03:51 +0200

    Traditional code

--- ViewControls/ViewController.swift
+++ ViewControls/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
+class Trip {
+
+    let departure: NSDate
+    let arrival: NSDate
+    let actualDeparture: NSDate
+
+    init(departure: NSDate, arrival: NSDate, actualDeparture: NSDate? = nil) {
+        self.departure = departure
+        self.arrival = arrival
+        self.actualDeparture = actualDeparture ?? departure
+    }
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var departureTimeLabel: UILabel!
+    @IBOutlet weak var arrivalTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var delayLabel: UILabel!
+
+    let trip = Trip(departure: NSDate(timeIntervalSince1970: 1444396193), arrival: NSDate(timeIntervalSince1970: 1444397193), actualDeparture: NSDate(timeIntervalSince1970: 1444396493))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        dateLabel.text = NSDateFormatter.localizedStringFromDate(trip.departure, dateStyle: .ShortStyle, timeStyle: .NoStyle)
+        departureTimeLabel.text = NSDateFormatter.localizedStringFromDate(trip.departure, dateStyle: .NoStyle, timeStyle: .ShortStyle)
+        arrivalTimeLabel.text = NSDateFormatter.localizedStringFromDate(trip.arrival, dateStyle: .NoStyle, timeStyle: .ShortStyle)
 
+        let durationFormatter = NSDateComponentsFormatter()
+        durationFormatter.allowedUnits = [.Hour, .Minute]
+        durationFormatter.unitsStyle = .Short
+        durationLabel.text = durationFormatter.stringFromDate(trip.departure, toDate: trip.arrival)
+
+        let delay = trip.actualDeparture.timeIntervalSinceDate(trip.departure)
+        if delay > 0 {
+            durationFormatter.unitsStyle = .Full
+            delayLabel.text = String.localizedStringWithFormat(NSLocalizedString("%@ delay", comment: "Show the delay"), durationFormatter.stringFromTimeInterval(delay)!)
+            departureTimeLabel.textColor = .redColor()
+        } else {
+            delayLabel.hidden = true
+        }
+    }
 
 }
 
